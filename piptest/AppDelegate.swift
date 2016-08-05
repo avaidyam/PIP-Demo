92d5b73c094f438346802bef09f0ce7c9d44ca29
Aditya Vaidyam <avaidyam@purdue.edu>
2016-08-05 14:15:18 -0400

    Initial Commit

---
+++ piptest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  piptest
+//
+//  Created by Aditya Vaidyam on 8/5/16.
+//  Copyright © 2016 Aditya Vaidyam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ piptest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  piptest
+//
+//  Created by Aditya Vaidyam on 8/5/16.
+//  Copyright © 2016 Aditya Vaidyam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
